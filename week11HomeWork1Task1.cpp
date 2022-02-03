f99c72ad3afe7ad8d0a74b40fb0b469557d9deab
Alexandr Hanmagomedov <shock2004@mail.ru>
2022-02-03 20:09:42 +0300

    Добавьте файлы проекта.

---
+++ week11HomeWork1Task1.cpp
@@ -0,0 +1,25 @@
+﻿/*
+Задание 1. Написать функцию, которая принимает два параметра:
+основание степени и показатель степени, и вычисляет степень числа на основе полученных данных.
+*/
+#include <iostream>
+using namespace std;
+
+int stepen4isla(int a, int b)
+{
+    int otv = 1;
+    for (int i = 0; i < b; i++)
+    {
+        otv *= a;
+    }
+    return otv;
+}
+void main()
+{
+    int a, b;
+    cout << "Vvedite 4islo ";
+    cin >> a;
+    cout << "Vvedite stepen` ";
+    cin >> b;
+    cout << "4islo  " << a << "  v stepeni  " << b << " = " << stepen4isla(a, b);
+}
